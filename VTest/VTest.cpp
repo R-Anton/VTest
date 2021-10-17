6647edd37d4b9be6701f15a51355ba743bb2f97c
Anton Rashevsky <ARashevsky@TOPCON.COM>
2021-10-18 00:22:44 +0300

    First running

---
+++ VTest/VTest.cpp
@@ -0,0 +1,26 @@
+﻿// VTest.cpp : Defines the entry point for the application.
+//
+#include "VTest.h"
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vulkan/vulkan.h>
+
+
+
+int main() {
+   HelloTriangleApplication app;
+
+   try {
+      app.run();
+
+   }
+   catch (const std::exception& e) {
+      std::cerr << e.what() << std::endl;
+      return EXIT_FAILURE;
+
+   }
+
+   return EXIT_SUCCESS;
+
+}
